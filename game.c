bf3c911a72e532ad164a656b2ab2302b876174fe
Aman-785 <aliamanahmed4@gmail.com>
2025-04-02 10:25:54 +0200

    nouveau

--- game.c
+++ game.c
@@ -3,23 +3,34 @@
 #include "charactere.h"
 #include "event.h"
 
-int jouer(SDL_Renderer* renderer) {
-    
-	//charger image et personnage. 
+int jouer(SDL_Renderer *renderer)
+{
 
-    int continuer = 1; //a utiliser pour savoir si on continue la boucle du jeu ou si on arrête. 
+    // charger image et personnage.
+
+    int continuer = 1; // a utiliser pour savoir si on continue la boucle du jeu ou si on arrête.
     SDL_Event events;
 
-    while(continuer){ //coeur du jeu ici, les actions seront repété pour faire le déplacement des différentes images, ...
-        
-        //gérer les différentes actions. 
-        
-        
+    while (continuer)
+    { // coeur du jeu ici, les actions seront repété pour faire le déplacement des différentes images, ...
+        int i = 0;
+        switch (i)
+        {
+        case 1:
+
+            
+
+            break;
+
+        case 2:
+
+            break;
+        }
     }
-    
-	//a vous de compléter, au fur et à mesure, les deux fonctions en dessous pour bien faire le nettoyage. 
-    //LibererMap(map, sprites);
-    //freePersonnage(mario, goomba, nbGoomba);
+
+    // a vous de compléter, au fur et à mesure, les deux fonctions en dessous pour bien faire le nettoyage.
+    // LibererMap(map, sprites);
+    // freePersonnage(mario, goomba, nbGoomba);
 
     return continuer;
 }
--- main.c
+++ main.c
@@ -2,15 +2,14 @@
 #include <SDL2/SDL_image.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "file.h"
 #include "game.h"
 
-// ligne de compilation gcc main.c file.c game.c event.c charactere.c -o mario -lSDL2main -lSDL2_image -lSDL2
+// ligne de compilation gcc main.c file.c game.c event.c charactere.c -o mario -lSDL2main -lSDL2_image1 -lSDL2
 
 int init(SDL_Window **window, SDL_Renderer **renderer, int w, int h);
 
-void image(SDL_Renderer *renderer)
+void image1(SDL_Renderer *renderer) // Image du main
 {
 	SDL_Rect rect2 = {0, 0, 600, 600};
 	SDL_Rect rect1 = {0, 0, 600, 600};
@@ -21,13 +20,59 @@ void image(SDL_Renderer *renderer)
 	SDL_RenderPresent(renderer);
 }
 
+void image2(SDL_Renderer *renderer) // Image crédit
+{
+	SDL_Rect rect2 = {0, 0, 600, 600};
+	SDL_Rect rect1 = {0, 0, 600, 600};
+
+	SDL_RenderClear(renderer);
+	SDL_Texture *texture = loadImage("Sans titre.jpeg", renderer);
+	SDL_RenderCopy(renderer, texture, &rect1, &rect2);
+	SDL_RenderPresent(renderer);
+}
+
 int main(int argc, char *argv[])
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_Window *window = SDL_CreateWindow("Mario", 700, 500, 600, 600, SDL_WINDOW_SHOWN);
 
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-	image(renderer);
-	SDL_Delay(5000);
+	image1(renderer);
+
+	SDL_Event event;
+	int continuer = 1;
+
+	while (SDL_PollEvent(&event) || continuer == 1) // Boucle infini
+	{
+
+		switch (event.type)
+		{
+		case SDL_QUIT:
+
+			continuer = 0;
+			break;
+
+		case SDL_KEYDOWN:
+
+			switch (event.key.keysym.sym)
+
+			{
+			case SDLK_1:
+
+				jouer(renderer);
+				
+				break;
+
+			case SDLK_2: // image crédit
+
+				image2(renderer);
+
+				break;
+			}
+		}
+		jouer(renderer);
+	}
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
 	SDL_Quit();
 }
